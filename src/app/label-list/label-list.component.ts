dea5f7425234d6a6cf9325b421cc53cafd26c195
unknown <a_saudabekov@kbtu.kz>
2023-12-11 18:11:02 +0300

    first commit

---
+++ src/app/label-list/label-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-label-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './label-list.component.html',
+  styleUrl: './label-list.component.css'
+})
+export class LabelListComponent {
+
+}
